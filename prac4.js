7bf0678742c3daa9113e7ad031743f68a6af7120
klimenkovnkt <klimenkovnkt@gmail.com>
2026-04-10 00:01:15 +0300

    Update prac4.js

--- prac4.js
+++ prac4.js
@@ -809,8 +809,43 @@ function endRoutineBegin(snapshot) {
     // keep track of which components have finished
     endComponents = [];
     endComponents.push(text_2);
-    
+
+
+      
     for (const thisComponent of endComponents)
+      if ('status' in thisComponent)
+        thisComponent.status = PsychoJS.Status.NOT_STARTED;
+    return Scheduler.Event.NEXT;
+
+    psychoJS._saveResults = 0;
+    
+    // Generate filename for results
+    let filename = psychoJS._experiment._experimentName + '_' + psychoJS._experiment._datetime + '.csv';
+    // Extract data object from experiment
+    let dataObj = psychoJS._experiment._trialsData;
+    // Convert data object to CSV
+    let data = [Object.keys(dataObj[0])].concat(dataObj).map(it => {
+        return Object.values(it).toString()
+    }).join('\n')
+    // Send data to OSF via DataPipe
+    console.log('Saving data...');
+    fetch('https://pipe.jspsych.org/api/data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: '*/*',
+         },   
+         body: JSON.stringify({
+            experimentID: 'OzU0rckSZdSj', // * UPDATE WITH YOUR DATAPIPE EXPERIMENT ID *
+            filename: filename, 
+            data: data,
+         }),
+    }).then(response => response.json()).then(data => {
+    // Log response and force experiment 
+        console.log(data);
+        quitPsychoJS();
+    })
+    for (const thisComponent of byeComponents)
       if ('status' in thisComponent)
         thisComponent.status = PsychoJS.Status.NOT_STARTED;
     return Scheduler.Event.NEXT;
